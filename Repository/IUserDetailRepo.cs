5cffe96bfc88ff1e73afa6575b646c687b50bed7
prannoy <prannoy.mulmi@gmail.com>
2017-10-05 16:37:11 +0200

    Added repository for the access of Individual, Organization and Hobbies

---
+++ Repository/IUserDetailRepo.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using App.Models;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace App.Repository
+{
+    public interface IUserDetailRepo
+    {
+        List<Hobbies> GetHobbies(string id);
+        List<Individual> GetIndividual(string id);
+        List<Organization> GetOrganization(string id);
+    }
+}    
---
+++ Repository/UserDetailRepo.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using App.Models;
+
+namespace App.Repository
+{
+    public class UserDetailRepo :IUserDetailRepo
+    {
+        private readonly ProfileContextDb _userRepo;
+
+        public UserDetailRepo(ProfileContextDb userRepo)
+        {
+            _userRepo = userRepo;
+        }
+        
+        public List<Hobbies> GetHobbies(string id)
+        {
+            return _userRepo.Hobbies.Where(x => x.AspNetUserId == id).ToList();
+            
+        }
+
+        public List<Individual> GetIndividual(string id)
+        {
+            return _userRepo.Indvidual.Where(x => x.AspNetUserId == id).ToList();
+        }
+
+        public List<Organization> GetOrganization(string id)
+        {
+            return _userRepo.Organization.Where(x => x.AspNetUserId == id).ToList();
+        }
+    }
+}
