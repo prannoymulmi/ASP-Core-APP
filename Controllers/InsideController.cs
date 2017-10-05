2bb67cb9a0652d4e6973ec02e57671b525d1c7f7
prannoy <prannoy.mulmi@gmail.com>
2017-10-05 16:37:43 +0200

    Added View and view models for the Inside View

--- Controllers/InsideController.cs
+++ Controllers/InsideController.cs
@@ -1,5 +1,7 @@
 ﻿using System.Threading.Tasks;
 using App.Models;
+using App.Repository;
+using App.ViewModels;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,16 +10,34 @@ namespace App.Controllers
     public class Inside : Controller
     {
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IUserDetailRepo _userRepo;
 
-        public Inside(SignInManager<ApplicationUser> signInManager)
+        public Inside(SignInManager<ApplicationUser> signInManager, IUserDetailRepo userRepo, UserManager<ApplicationUser> userManager)
         {
-            _signInManager = signInManager;    
+            _signInManager = signInManager;
+            _userRepo = userRepo;
+            _userManager = userManager;
         }
         // GET
         public IActionResult Index()
         {
-           
-            return View();
+            var userId = _userManager.GetUserId(HttpContext.User);
+            var hobbies = _userRepo.GetHobbies(userId);
+
+            var individual = _userRepo.GetIndividual(userId);
+
+            var organization = _userRepo.GetOrganization(userId);
+
+            var model = new InsideViewModel
+            {
+                Individuals = individual,
+                Organizations = organization,
+                Hobbies = hobbies
+
+            };
+
+            return View(model);
         }
         
         
---
+++ ViewModels/InsideViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using App.Models;
+
+namespace App.ViewModels
+{
+    public class InsideViewModel
+    {
+        public List<Individual> Individuals { get; set; }
+        public List<Hobbies> Hobbies { get; set; }
+        public List<Organization> Organizations { get; set; }
+    }
+}
